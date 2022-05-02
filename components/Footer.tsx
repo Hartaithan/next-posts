9ed2b62787feebc62122288660f0d643a2b7766c
hartaithan <hartaithan@gmail.com>
2022-05-02 13:58:18 +0900

    use container in header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import Container from "./Container";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <Container>
         <div className="relative flex items-center justify-end h-16">
           <Link href="https://hartaithan.github.io/">
             <a className="text-white" target="_blank" rel="noopener noreferrer">
@@ -12,7 +13,7 @@ const Footer: React.FC = () => {
             </a>
           </Link>
         </div>
-      </div>
+      </Container>
     </footer>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import clsx from "clsx";
 import Link from "next/link";
+import Container from "./Container";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -16,7 +17,7 @@ const Header: React.FC = () => {
     <Disclosure as="header" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <Container>
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -117,8 +118,7 @@ const Header: React.FC = () => {
                 </Menu>
               </div>
             </div>
-          </div>
-
+          </Container>
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
