63c08ed4d896ad9e5c1a33f8a974e50a6f4a74bb
hartaithan <hartaithan@gmail.com>
2022-06-18 16:47:55 +0900

    api/comments add comments route

---
+++ pages/api/comments/index.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../../utils/supabaseClient";
+
+async function addComment(req: NextApiRequest, res: NextApiResponse) {
+  const { body } = req;
+  const payload = {
+    post_id: body.post_id,
+    content: body.content,
+    user: body.user,
+  };
+  const { data, error } = await supabase
+    .from("comments")
+    .insert([payload])
+    .single();
+  if (error) {
+    return res
+      .status(400)
+      .json({ message: "Error creating new comment", error: error });
+  }
+  return res
+    .status(201)
+    .json({ message: "Comment successfully created", comment: data });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+  switch (method) {
+    case "POST":
+      return addComment(req, res);
+    default:
+      res.setHeader("Allow", ["POST"]);
+      return res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
