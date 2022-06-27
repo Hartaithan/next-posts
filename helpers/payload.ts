3a41be36becb4c030821b7ab6e906a9c46e08ec7
hartaithan <hartaithan@gmail.com>
2022-06-27 11:42:40 +0900

    trim string values in validate

--- helpers/payload.ts
+++ helpers/payload.ts
@@ -6,7 +6,7 @@ export const validatePayload = (payload: IPayloadObject) => {
   const results = [];
   for (var key in payload) {
     const value = payload[key] as string;
-    if (!value || value.length === 0) {
+    if (!value || value.trim().length === 0) {
       results.push(key + " is empty");
     }
   }
